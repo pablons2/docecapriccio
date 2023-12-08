06fb131d9eec4a37843ef649d9f27e8d97652afe
pablons2 <pablonicholas2012@gmail.com>
2023-12-08 00:48:56 -0300

    first commit

---
+++ static/js/js.js
@@ -0,0 +1,17 @@
+const btns_menu = document.querySelector('#btns_menu');
+
+const buttons = [
+    {
+    'value': 'CARDÁPIO',
+    'link':'static/docs/menu.pdf'
+    },
+    {
+    'value': 'ENCOMENDAR',
+    'link':'https://wa.me/5582998280216?text=ol%C3%A1%2C+desejo+encomendar+um+delicioso+Chocopriccio%21'
+    }
+]
+
+buttons.map((item)=>{
+    btns_menu.innerHTML += `<a href="${item.link}"> ${item.value} </a>`
+
+})
